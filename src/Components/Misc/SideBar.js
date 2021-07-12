e19650588b879705a0c8332dae804339947e1588
Garvit-121172 <43706077+Garvit-121172@users.noreply.github.com>
2021-07-12 23:53:47 +0530

    In Progress

--- src/Components/Misc/SideBar.js
+++ src/Components/Misc/SideBar.js
@@ -3,15 +3,19 @@ import HomeIcon from '@material-ui/icons/Home';
 import SettingsIcon from '@material-ui/icons/Settings';
 import DescriptionIcon from '@material-ui/icons/Description';
 import {NavLink} from 'react-router-dom';
+import Homeicon from './../../Homeicon.svg'
+
 function SideBar() {
     return (
         <div className="sidebar">
+            <div className="sidebar-list">
             <ul>
-                <li><NavLink exact activeClassName="active" to="/" > <div className="side-icon" ><HomeIcon /></div> </NavLink>  <div>Home</div></li>
+                <li><svg xmlns={Homeicon} style={{backgroundColor:"red",height:"30px",width:"30px",color:"black"}} ></svg></li>
+                <li><div className="list-item"><NavLink exact activeClassName="active" to="/" > <div className="side-icon" ><HomeIcon /></div> </NavLink></div><div>Home</div></li>
                 <li><NavLink exact activeClassName="active" to="/control"><div className="side-icon"><SettingsIcon /></div> </NavLink> <div>Controls</div></li>
                 <li><NavLink exact activeClassName="active" to="/report"><div className="side-icon"><DescriptionIcon /></div> </NavLink><div>Reports</div></li>
-
             </ul>
+            </div>
         </div>
     )
 }
--- src/Components/OnBoard/OnBoardLeft.js
+++ src/Components/OnBoard/OnBoardLeft.js
@@ -3,7 +3,7 @@ import Onboardleft from '../../OnBoardLeft.svg'
 function OnBoardLeft() {
     return (
         <div className="onboard-left" >
-        <img src={Onboardleft}  alt="left-pic" height="550px" widht="350px" />
+        <img src={Onboardleft}  alt="left-pic" height="450px" width="480px" />
         </div>
     )
 }
